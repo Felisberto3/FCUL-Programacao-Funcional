f3a8c29795f24eed35dc41f1a029b136974d39be
Felisberto Alberto <felisbertoalberto183@gmail.com>
2026-03-07 22:20:07 +0000

    ex17

---
+++ Ficha1/ex13.hs
@@ -0,0 +1,3 @@
+-- 13. Defina a lista infinita com todas as potências de dois.
+
+listInfinita = [2 ^ x | x <- [1 ..]]
---
+++ Ficha1/ex7.hs
@@ -0,0 +1,6 @@
+-- 7. Escreva uma função que divide o intervalo entre dois valores em n
+-- partições iguais. O resultado deverá ser uma lista de n+1 elementos
+-- onde a primeira partição é dada pelos primeiros dois elementos da lista,
+-- a segunda partição pelo segundo e terceiro elementos da lista, e assim
+-- adiante. Por exemplo:
+particao a b n = [a + ((b - a) / n) * x | x <- [0 .. n]]
