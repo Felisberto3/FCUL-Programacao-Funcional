34ff82f338f3774b870622444ecdd860012e3b35
Felisberto Alberto <felisbertoalberto183@gmail.com>
2026-05-21 08:03:52 +0100

    module

--- Taining/Module/Self.hs
+++ Taining/Module/Self.hs
@@ -1,4 +1,6 @@
-module Self (introduce) where
+module Self (introduce, Tree (..)) where
+
+data Tree a = Folha | No (Tree a) a (Tree a) deriving (Show)
 
 introduce :: String -> String
 introduce n = "Meu nome e " ++ n
