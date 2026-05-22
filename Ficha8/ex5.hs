4a05914903180e3978db42e53250cd822f72b381
Felisberto Alberto <felisbertoalberto183@gmail.com>
2026-05-22 06:13:59 +0100

    sortear binario

---
+++ Ficha8/ex5.hs
@@ -0,0 +1,13 @@
+guess :: Int -> IO ()
+guess n = do
+  sortear 0 n 1
+  where
+    sortear :: Int -> Int -> Int -> IO ()
+    sortear i f acc = do
+      let v = i + ((f - i) `div` 2)
+      putStr $ show v ++ "?"
+      c <- getLine
+      case c of
+        ">" -> sortear v f (acc + 1)
+        "<" -> sortear i v (acc + 1)
+        "=" -> putStrLn $ "Sucesso apos " ++ show acc ++ " tentativas "
